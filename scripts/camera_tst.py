e83e01534e69934c11391204728f75c1334626e1
Yau Shing <75722100+yaushing@users.noreply.github.com>
2024-11-01 07:47:48 +0800

    clear all

--- scripts/camera_tst.py
+++
@@ -1,9 +0,0 @@
-import time
-import picamera
-
-with picamera.PiCamera() as camera:
-        camera.start_preview()
-        print('preview started')
-        time.sleep(10)
-        camera.stop_preview()
-        print('preview ended')
